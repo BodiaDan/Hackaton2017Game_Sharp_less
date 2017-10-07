61881791d8b00fbf31ed0796dd65cb3beb5abc52
BodiaDan <32496963+BodiaDan@users.noreply.github.com>
2017-10-07 23:50:47 +0300

    push5

--- Hackathon/New Unity Project 2/Assets/Script/UIElements.cs
+++ Hackathon/New Unity Project 2/Assets/Script/UIElements.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class UIElements : MonoBehaviour {
 
+	public GameObject buttonA; 
+	public GameObject buttonD; 
+	public GameObject buttonW; 
+	public GameObject buttonS; 
 	public GameObject buttonE; 
+	public GameObject buttonSpace; 
+
 
 	// Use this for initialization
 	void Start () {
+		buttonE.SetActive (false);
+		buttonS.SetActive (false);
+		buttonSpace.SetActive (false);
+		buttonW.SetActive (false);
 	}
 	
 	// Update is called once per frame
