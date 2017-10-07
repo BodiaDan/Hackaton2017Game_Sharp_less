a56acc875d4f266bf874789d40fc5252da809739
BodiaDan <32496963+BodiaDan@users.noreply.github.com>
2017-10-07 19:36:37 +0300

    push3

---
+++ Hackathon/New Unity Project 2/Assets/Script/DeltaplannerMoveController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaplannerMoveController : MonoBehaviour {
+	public float deltaPlanSpeed = 20f;
+	public float upForce = 200f;
+
+	public float rotateNumber = 0.3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += Vector3.forward * deltaPlanSpeed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.D)) {
+			float tempRatateX = transform.rotation.x + rotateNumber * 2.5f;
+			if (tempRatateX > 30f) {
+				tempRatateX = 30f;
+			} else if (tempRatateX < -30f) {
+				tempRatateX = -30f;
+			}
+			float tempRatateY = transform.rotation.x - rotateNumber * 2.0f;
+			if (tempRatateY > 40f) {
+				tempRatateY = 40f;
+			} else if (tempRatateX < -40f) {
+				tempRatateY = -40f;
+			}
+			gameObject.transform.Rotate ( new Vector3 (0, tempRatateX, tempRatateY ));
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			float tempRatateX = transform.rotation.x - rotateNumber * 2.5f;
+			if (tempRatateX > 30f) {
+				tempRatateX = 30f;
+			} else if (tempRatateX < -30f) {
+				tempRatateX = -30f;
+			}
+			float tempRatateY = transform.rotation.x + rotateNumber * 2.0f;
+			if (tempRatateY > 40f) {
+				tempRatateY = 40f;
+			} else if (tempRatateX < -40f) {
+				tempRatateY = -40f;
+			}
+			gameObject.transform.Rotate ( new Vector3 (0, tempRatateX, tempRatateY ));
+
+			//gameObject.transform.Rotate ( new Vector3 (0, Mathf.Clamp(transform.rotation.x - rotateNumber * 2.5f,-30, 30 ), Mathf.Clamp(transform.rotation.z + rotateNumber,-40, 40 ) ));		
+		}
+		if (transform.rotation.x < 19) {
+			gameObject.transform.Rotate (new Vector3 (0.5f, 0, 0));
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "UpPiont")
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3( 0, upForce, 0));
+		}
+		if (other.transform.tag == "Ground")
+		{
+			Destroy (gameObject);
+		}
+
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.transform.tag == "Ground")
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		
+		if (other.transform.tag == "UpPiont" && transform.rotation.x > -19f) {
+			transform.Rotate (new Vector3 (-1f, 0, 0));
+		}
+	}
+}
--- Hackathon/New Unity Project 2/Assets/Script/MovePlayer.cs
+++ Hackathon/New Unity Project 2/Assets/Script/MovePlayer.cs
@@ -5,46 +5,113 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour {
 
 	public float speedPlayer = 20f;
-	public bool isInJump = false;
 	public float upForce = 200f;
-	// Use this for initialization
+	public bool isInJump = false;
+
+	public bool isHide = false;
+	public Vector3 previousPosition;
+
+	public float stateParameter = 0f;
+	public int state = 0; // 0- stay 1 -lying 2- jump
+	public bool blockChangeAnim = false;
+	public Animator animator;
 	void Start () {
-		
+		animator = GetComponent<Animator> ();
 	}
 
     // Update is called once per frame
 
     void Update () {
+		if (!blockChangeAnim) {
+			switch (state) {
+			case 0:
+				stateParameter = 0f;
+				break;
+			case 1: 
+				stateParameter = 0.375f;
+				break;
+			case 2:
+				stateParameter = 0.75f;
+				break;
+			}
+		}
+
 		if (Input.GetKey (KeyCode.D)) {
 			transform.position += Vector3.right * speedPlayer * Time.deltaTime;
+
 		}
 		if (Input.GetKey (KeyCode.A)) {
 			transform.position += Vector3.left * speedPlayer * Time.deltaTime;
 		}
-		if (Input.GetKey (KeyCode.W) && !isInJump) {
-			Debug.Log ("jump");
+		if (Input.GetKey (KeyCode.W) && !isInJump && state != 1) {
 			isInJump = true;
+			state = 2;
+			StartCoroutine ("GoJump");
 			GetComponent<Rigidbody>().AddForce(new Vector3( 0, upForce, 0));
 		}
-		if (Input.GetKey (KeyCode.S) && !isInJump) {
-			Debug.Log ("duck");
+		if (Input.GetKeyDown (KeyCode.S) && !isInJump) {
 		}
+
+		if (!blockChangeAnim) {
+			animator.SetFloat ("stateParameter", stateParameter);
+		}
+	}
+
+	IEnumerator GoJump() {
+		stateParameter = 0.625f;
+		animator.SetFloat ("stateParameter", stateParameter);
+		yield return new WaitForSeconds (0.417f);
+		stateParameter = 0.75f;
+		animator.SetFloat ("stateParameter", stateParameter);
+		blockChangeAnim = false;
 	}
 
     void OnCollisionEnter(Collision collision) {
 		Debug.Log (collision.transform.tag);
 		if (collision.transform.root.tag == "Ground") {
 			isInJump = false;
+			StopCoroutine ("GoJump");
+			stateParameter = 0;
+			state = 0;
+			blockChangeAnim = false;
 		}
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "DeathZone")
-        {
-            Destroy(obj: gameObject);
+		if (other.transform.tag == "DeathZone") {
+			Destroy(obj: gameObject);
         }
-        
+		if (other.transform.tag == "HideZone") {
+			GameObject.Find ("UIController").GetComponent<UIElements> ().buttonE.SetActive (true);	
+		}
     }
+
+	void OnTriggerStay(Collider other) {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			if (!isHide) {
+				isHide = true;
+				HidePlayer (other.transform.position);
+			} else {
+				isHide = false;
+				ShowPlayer ();
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.transform.tag == "HideZone") {
+			GameObject.Find ("UIController").GetComponent<UIElements> ().buttonE.SetActive (false);
+		}
+	}
+
+	void HidePlayer(Vector3 newPos) {
+		previousPosition = transform.position;
+		transform.position = new Vector3 (transform.position.x, transform.position.y, newPos.z + 2f );
+	}
+
+	void ShowPlayer() {
+		transform.position = previousPosition;
+	}
 
 }
